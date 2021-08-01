fc01305dc17e02cd024b8b52b7d231e69977afb4
anko9801 <daiusa3@icloud.com>
2021-08-02 01:39:22 +0900

    queryHaveReadの余計なカラムを削除

--- webapp/go/src/isubata/app.go
+++ webapp/go/src/isubata/app.go
@@ -417,16 +417,9 @@ func queryChannels() ([]int64, error) {
 }
 
 func queryHaveRead(userID, chID int64) (int64, error) {
-	type HaveRead struct {
-		UserID    int64     `db:"user_id"`
-		ChannelID int64     `db:"channel_id"`
-		MessageID int64     `db:"message_id"`
-		UpdatedAt time.Time `db:"updated_at"`
-		CreatedAt time.Time `db:"created_at"`
-	}
-	h := HaveRead{}
+	var MessageID int64
 
-	err := db.Get(&h, "SELECT * FROM haveread WHERE user_id = ? AND channel_id = ?",
+	err := db.Get(&MessageID, "SELECT message_id FROM haveread WHERE user_id = ? AND channel_id = ?",
 		userID, chID)
 
 	if err == sql.ErrNoRows {
@@ -434,7 +427,7 @@ func queryHaveRead(userID, chID int64) (int64, error) {
 	} else if err != nil {
 		return 0, err
 	}
-	return h.MessageID, nil
+	return MessageID, nil
 }
 
 func fetchUnread(c echo.Context) error {
@@ -722,8 +715,6 @@ func tRange(a, b int64) []int64 {
 }
 
 func main() {
-	fmt.Printf("WAKARANZOU")
-	log.Printf("WAKARANXOU")
 	e := echo.New()
 	funcs := template.FuncMap{
 		"add":    tAdd,
