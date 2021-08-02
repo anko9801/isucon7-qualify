897fbd891e20a1f8dbbd81385519350cd4166fe1
anko9801 <daiusa3@icloud.com>
2021-08-02 10:45:14 +0900

    fix pre

--- webapp/go/src/isubata/app.go
+++ webapp/go/src/isubata/app.go
@@ -383,8 +383,8 @@ func getMessage(c echo.Context) error {
 	}
 
 	type tmp struct {
-		MessageID   int64     `db:"id"`
-		UserID      int64     `db:"id"`
+		MessageID   int64     `db:"message_id"`
+		UserID      int64     `db:"user_id"`
 		Name        string    `db:"name"`
 		DisplayName string    `db:"display_name"`
 		AvatarIcon  string    `db:"avatar_icon"`
@@ -392,7 +392,7 @@ func getMessage(c echo.Context) error {
 		Content     string    `db:"content"`
 	}
 	data := []tmp{}
-	err = db.Select(&data, "SELECT M.id, U.id, U.name, U.display_name, U.avatar_icon, M.created_at, M.content FROM user AS U JOIN (SELECT id, user_id FROM message WHERE channel_id = ? AND id > ? ORDER BY id DESC LIMIT 100) AS M ON U.id = M.user_id", chanID, lastID)
+	err = db.Select(&data, "SELECT M.id AS message_id, U.id AS user_id, U.name, U.display_name, U.avatar_icon, M.created_at, M.content FROM user AS U JOIN (SELECT id, user_id FROM message WHERE channel_id = ? AND id > ? ORDER BY id DESC LIMIT 100) AS M ON U.id = M.user_id", chanID, lastID)
 	if err != nil {
 		return err
 	}
