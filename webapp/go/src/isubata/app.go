05ff532ae586e39edd624fdc261a381f465cb50a
anko9801 <daiusa3@icloud.com>
2021-08-10 03:29:27 +0900

    fix

--- webapp/go/src/isubata/app.go
+++ webapp/go/src/isubata/app.go
@@ -81,7 +81,7 @@ func init() {
 	db.SetMaxIdleConns(1024) // デフォルトだと2
 	db.SetConnMaxLifetime(0) // 一応セット
 	// db.SetConnMaxIdleTime(0)
-	db.SetMaxOpenConns(20)
+	// db.SetMaxOpenConns(20)
 	// db.SetConnMaxLifetime(5 * time.Minute)
 	log.Printf("Succeeded to connect db.")
 }
